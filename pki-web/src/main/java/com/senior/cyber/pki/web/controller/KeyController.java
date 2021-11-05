856e0625892ff3695d0e9b56ba9e2b67070b3798
Senior Cyber <senior.cyber@gmail.com>
2021-11-06 04:12:47 +0700

    public key

--- pki-web/src/main/java/com/senior/cyber/pki/web/controller/KeyController.java
+++ pki-web/src/main/java/com/senior/cyber/pki/web/controller/KeyController.java
@@ -48,147 +48,139 @@ public class KeyController {
     @Autowired
     protected SslConfiguration sslConfiguration;
 
-    @RequestMapping(path = "/info", method = RequestMethod.GET, produces = "text/plain")
-    public ResponseEntity<String> info(HttpServletRequest request) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableEntryException {
-        if (request.isSecure()) {
-            KeyStore keyStore = KeyStore.getInstance(sslConfiguration.getKeyStoreType());
-            try (InputStream stream = new ByteArrayInputStream(IOUtils.toByteArray(FileUtils.openInputStream(sslConfiguration.getKeyStore())))) {
-                keyStore.load(stream, sslConfiguration.getKeyStorePassword().toCharArray());
-            }
-            String keyAlias = null;
-            if (sslConfiguration.getKeyAlias() == null || "".equals(sslConfiguration.getKeyAlias())) {
-                Enumeration<String> aliases = keyStore.aliases();
-                while (aliases.hasMoreElements()) {
-                    keyAlias = aliases.nextElement();
-                    break;
-                }
-            } else {
-                keyAlias = sslConfiguration.getKeyAlias();
-            }
-            Certificate certificate = keyStore.getCertificate(keyAlias);
-            PublicKey publicKey = PublicKeyUtils.read(PublicKeyUtils.write(certificate.getPublicKey()));
-            return ResponseEntity.ok(PublicKeyUtils.write(publicKey));
-        } else {
-            throw new java.lang.UnsupportedOperationException("https configuration is required");
+    protected static KeyPair keyPair;
+
+    protected static KeyPair lookupKeyPair(SslConfiguration sslConfiguration) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        if (keyPair != null) {
+            return keyPair;
         }
+        KeyStore keyStore = KeyStore.getInstance(sslConfiguration.getKeyStoreType());
+        String keyPassword = null;
+        if (sslConfiguration.getKeyPassword() == null || "".equals(sslConfiguration.getKeyPassword())) {
+            keyPassword = sslConfiguration.getKeyStorePassword();
+        } else {
+            keyPassword = sslConfiguration.getKeyPassword();
+        }
+        try (InputStream stream = new ByteArrayInputStream(IOUtils.toByteArray(FileUtils.openInputStream(sslConfiguration.getKeyStore())))) {
+            keyStore.load(stream, sslConfiguration.getKeyStorePassword().toCharArray());
+        }
+        String keyAlias = null;
+        if (sslConfiguration.getKeyAlias() == null || "".equals(sslConfiguration.getKeyAlias())) {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                keyAlias = aliases.nextElement();
+                break;
+            }
+        } else {
+            keyAlias = sslConfiguration.getKeyAlias();
+        }
+        Certificate certificate = keyStore.getCertificate(keyAlias);
+        PublicKey publicKey = PublicKeyUtils.read(PublicKeyUtils.write(certificate.getPublicKey()));
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(keyAlias, keyPassword.toCharArray());
+        keyPair = new KeyPair(publicKey, privateKey);
+        return keyPair;
+    }
+
+    protected static KeyPair lookupKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
+        if (keyPair != null) {
+            return keyPair;
+        }
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+        generator.initialize(256);
+        keyPair = generator.generateKeyPair();
+        return keyPair;
+    }
+
+    @RequestMapping(path = "/info", method = RequestMethod.GET, produces = "text/plain")
+    public ResponseEntity<String> info(HttpServletRequest request) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableEntryException, NoSuchProviderException {
+        KeyPair keyPair = null;
+        if (request.isSecure()) {
+            keyPair = lookupKeyPair(sslConfiguration);
+        } else {
+            keyPair = lookupKeyPair();
+        }
+        return ResponseEntity.ok(PublicKeyUtils.write(keyPair.getPublic()));
     }
 
     @RequestMapping(path = "/{clientId}/encrypt", method = RequestMethod.POST)
     public ResponseEntity<byte[]> encrypt(
             @RequestHeader("Client-Secret") String clientSecret,
-            @RequestHeader(value = "Public-Key", required = false) String publicKey,
+            @RequestHeader(value = "Public-Key") String clientPublicKeyText,
             @PathVariable("clientId") String clientId,
             HttpServletRequest request
     ) throws IOException, GeneralSecurityException {
-        if (request.isSecure() && (publicKey == null || "".equals(publicKey))) {
-            throw new java.lang.IllegalArgumentException("Public-Key is required");
-        }
-        if (!request.isSecure()) {
-            Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
-            Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-            SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
-            SecretKey secretKey = factory.generateSecret(secretKeySpec);
-
-            String kekJson = crypto.decrypt(secretKey, key.getKek());
-            KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
-            Aead aead = handle.getPrimitive(Aead.class);
-
-            byte[] requestPlainData = IOUtils.toByteArray(request.getInputStream());
-            byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
-            byte[] responsePlainData = aead.encrypt(requestPlainData, associatedData);
-            return ResponseEntity.ok(responsePlainData);
+        KeyPair keyPair = null;
+        if (request.isSecure()) {
+            keyPair = lookupKeyPair(sslConfiguration);
         } else {
-            KeyStore keyStore = KeyStore.getInstance(sslConfiguration.getKeyStoreType());
-            try (InputStream stream = FileUtils.openInputStream(sslConfiguration.getKeyStore())) {
-                keyStore.load(stream, sslConfiguration.getKeyStorePassword().toCharArray());
-            }
-            PrivateKey privateKey = (PrivateKey) keyStore.getKey(sslConfiguration.getKeyAlias(), sslConfiguration.getKeyPassword().toCharArray());
-            PublicKey clientPublicKey = PublicKeyUtils.read(crypto.decrypt(privateKey, publicKey));
-            SecretKey secret = crypto.lookupKeyAgreement((ECPrivateKey) privateKey, (ECPublicKey) clientPublicKey);
-
-            Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
-            Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-            SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
-            SecretKey secretKey = factory.generateSecret(secretKeySpec);
-
-            String kekJson = crypto.decrypt(secretKey, key.getKek());
-            KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
-            Aead aead = handle.getPrimitive(Aead.class);
-
-            String requestCipherText = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
-            String requestPlainText = crypto.decrypt(secret, requestCipherText);
-            byte[] requestPlainData = Base64.getDecoder().decode(requestPlainText);
-
-            byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
-            byte[] responsePlainData = aead.encrypt(requestPlainData, associatedData);
-            String responsePlainText = Base64.getEncoder().encodeToString(responsePlainData);
-            String responseCipherText = this.crypto.encrypt(secret, responsePlainText);
-            return ResponseEntity.ok(responseCipherText.getBytes(StandardCharsets.UTF_8));
+            keyPair = lookupKeyPair();
         }
+
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey clientPublicKey = PublicKeyUtils.read(crypto.decrypt(privateKey, clientPublicKeyText));
+        SecretKey secret = crypto.lookupKeyAgreement((ECPrivateKey) privateKey, (ECPublicKey) clientPublicKey);
+
+        Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
+        Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
+        SecretKey secretKey = factory.generateSecret(secretKeySpec);
+
+        String kekJson = crypto.decrypt(secretKey, key.getKek());
+        KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
+        Aead aead = handle.getPrimitive(Aead.class);
+
+        String requestCipherText = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+        String requestPlainText = crypto.decrypt(secret, requestCipherText);
+        byte[] requestPlainData = Base64.getDecoder().decode(requestPlainText);
+
+        byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
+        byte[] responsePlainData = aead.encrypt(requestPlainData, associatedData);
+        String responsePlainText = Base64.getEncoder().encodeToString(responsePlainData);
+        String responseCipherText = this.crypto.encrypt(secret, responsePlainText);
+        return ResponseEntity.ok(responseCipherText.getBytes(StandardCharsets.UTF_8));
     }
 
     @RequestMapping(path = "/{clientId}/decrypt", method = RequestMethod.POST)
     public ResponseEntity<byte[]> decrypt(
             @RequestHeader("Client-Secret") String clientSecret,
-            @RequestHeader(value = "Public-Key", required = false) String publicKey,
+            @RequestHeader(value = "Public-Key") String clientPublicKeyText,
             @PathVariable("clientId") String clientId,
             HttpServletRequest request
     ) throws IOException, GeneralSecurityException {
-        if (request.isSecure() && (publicKey == null || "".equals(publicKey))) {
-            throw new java.lang.IllegalArgumentException("Public-Key is required");
-        }
-        if (!request.isSecure()) {
-            Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
-            Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-            SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
-            SecretKey secretKey = factory.generateSecret(secretKeySpec);
-
-            String kekJson = crypto.decrypt(secretKey, key.getKek());
-
-            KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
-            Aead aead = handle.getPrimitive(Aead.class);
-
-            byte[] requestPlainData = IOUtils.toByteArray(request.getInputStream());
-            byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
-            byte[] responsePlainData = aead.decrypt(requestPlainData, associatedData);
-            return ResponseEntity.ok(responsePlainData);
+        KeyPair keyPair = null;
+        if (request.isSecure()) {
+            keyPair = lookupKeyPair(sslConfiguration);
         } else {
-            KeyStore keyStore = KeyStore.getInstance(sslConfiguration.getKeyStoreType());
-            try (InputStream stream = FileUtils.openInputStream(sslConfiguration.getKeyStore())) {
-                keyStore.load(stream, sslConfiguration.getKeyStorePassword().toCharArray());
-            }
-            PrivateKey privateKey = (PrivateKey) keyStore.getKey(sslConfiguration.getKeyAlias(), sslConfiguration.getKeyPassword().toCharArray());
-            PublicKey clientPublicKey = PublicKeyUtils.read(crypto.decrypt(privateKey, publicKey));
-            SecretKey secret = crypto.lookupKeyAgreement((ECPrivateKey) privateKey, (ECPublicKey) clientPublicKey);
-
-            Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
-            Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-            SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
-            SecretKey secretKey = factory.generateSecret(secretKeySpec);
-
-            String kekJson = crypto.decrypt(secretKey, key.getKek());
-
-            KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
-            Aead aead = handle.getPrimitive(Aead.class);
-
-            String requestCipherText = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
-            String requestPlainText = crypto.decrypt(secret, requestCipherText);
-            byte[] requestPlainData = Base64.getDecoder().decode(requestPlainText);
-
-            byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
-            byte[] responsePlainData = aead.decrypt(requestPlainData, associatedData);
-            String responsePlainText = Base64.getEncoder().encodeToString(responsePlainData);
-            String responseCipherText = this.crypto.encrypt(secret, responsePlainText);
-            return ResponseEntity.ok(responseCipherText.getBytes(StandardCharsets.UTF_8));
+            keyPair = lookupKeyPair();
         }
+
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey clientPublicKey = PublicKeyUtils.read(crypto.decrypt(privateKey, clientPublicKeyText));
+        SecretKey secret = crypto.lookupKeyAgreement((ECPrivateKey) privateKey, (ECPublicKey) clientPublicKey);
+
+        Optional<Key> optionalKey = keyRepository.findByClientId(clientId);
+        Key key = optionalKey.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(Base64.getDecoder().decode(clientSecret), "AES");
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("AES", BouncyCastleProvider.PROVIDER_NAME);
+        SecretKey secretKey = factory.generateSecret(secretKeySpec);
+
+        String kekJson = crypto.decrypt(secretKey, key.getKek());
+
+        KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withString(kekJson));
+        Aead aead = handle.getPrimitive(Aead.class);
+
+        String requestCipherText = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+        String requestPlainText = crypto.decrypt(secret, requestCipherText);
+        byte[] requestPlainData = Base64.getDecoder().decode(requestPlainText);
+
+        byte[] associatedData = "".getBytes(StandardCharsets.UTF_8);
+        byte[] responsePlainData = aead.decrypt(requestPlainData, associatedData);
+        String responsePlainText = Base64.getEncoder().encodeToString(responsePlainData);
+        String responseCipherText = this.crypto.encrypt(secret, responsePlainText);
+        return ResponseEntity.ok(responseCipherText.getBytes(StandardCharsets.UTF_8));
     }
 
 }
