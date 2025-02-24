6e6ac5014d8170e33032ee18c3e93c17fc3b2b36
Socheat KHAUV <pkayjava@gmail.com>
2025-02-24 14:07:10 +0700

    clean up

--- pki-dao/src/main/java/com/senior/cyber/pki/dao/dialect/MySQLDialect.java
+++ pki-dao/src/main/java/com/senior/cyber/pki/dao/dialect/MySQLDialect.java
@@ -5,7 +5,7 @@ import org.hibernate.dialect.DatabaseVersion;
 public class MySQLDialect extends org.hibernate.dialect.MySQLDialect {
 
     public MySQLDialect() {
-        super(DatabaseVersion.make(8));
+        super(DatabaseVersion.make(9));
     }
 
 }
