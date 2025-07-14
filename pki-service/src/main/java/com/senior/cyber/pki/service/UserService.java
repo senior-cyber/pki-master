9f7d12d07aba0f861de651a09d8148151dff0628
Socheat KHAUV <pkayjava@gmail.com>
2025-07-14 10:12:06 +0700

    more

--- pki-service/src/main/java/com/senior/cyber/pki/service/UserService.java
+++ pki-service/src/main/java/com/senior/cyber/pki/service/UserService.java
@@ -4,6 +4,7 @@ import com.google.common.io.BaseEncoding;
 import com.senior.cyber.pki.dao.entity.rbac.User;
 import com.senior.cyber.pki.dao.repository.rbac.UserRepository;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.jasypt.util.password.PasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +27,11 @@ public class UserService {
         if (authorization == null || authorization.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "access denied");
         }
-        if (!StringUtils.startsWithIgnoreCase(authorization, "Basic ")) {
+        if (!Strings.CS.startsWith(authorization, "Basic ")) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "access denied");
         }
         String loginAndPassword = new String(BaseEncoding.base64().decode(authorization.substring("Basic ".length())), StandardCharsets.UTF_8);
-        int colon = StringUtils.indexOf(loginAndPassword, ":");
+        int colon = Strings.CS.indexOf(loginAndPassword, ":");
         String login = StringUtils.substring(loginAndPassword, 0, colon);
         String password = StringUtils.substring(loginAndPassword, colon + 1);
         User user = this.userRepository.findByLogin(login);
@@ -44,7 +45,7 @@ public class UserService {
         }
 
         try {
-            if (!StringUtils.equals(user.getPassword(), password)) {
+            if (!Strings.CS.equals(user.getPassword(), password)) {
                 throw new ResponseStatusException(HttpStatus.FORBIDDEN, "access denied");
             }
 //            if (!this.passwordEncryptor.checkPassword(password, user.getPassword())) {
