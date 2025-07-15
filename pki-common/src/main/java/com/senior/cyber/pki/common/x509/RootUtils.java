2d799c7395df3678a57e2295148facc17df4f703
Socheat KHAUV <pkayjava@gmail.com>
2025-07-15 19:50:24 +0700

    more

--- pki-common/src/main/java/com/senior/cyber/pki/common/x509/RootUtils.java
+++ pki-common/src/main/java/com/senior/cyber/pki/common/x509/RootUtils.java
@@ -72,18 +72,12 @@ public class RootUtils {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
         try {
-            // builder.addExtension(Extension.keyUsage, keyUsageCritical, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.cRLSign | KeyUsage.keyCertSign));
             builder.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.cRLSign | KeyUsage.keyCertSign));
         } catch (CertIOException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
         try {
             builder.addExtension(Extension.subjectKeyIdentifier, false, utils.createSubjectKeyIdentifier(publicKey));
-        } catch (CertIOException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
-        }
-        try {
-            builder.addExtension(Extension.authorityKeyIdentifier, false, utils.createAuthorityKeyIdentifier(publicKey));
         } catch (CertIOException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
@@ -103,6 +97,68 @@ public class RootUtils {
         ContentSigner contentSigner = null;
         try {
             contentSigner = contentSignerBuilder.build(privateKey);
+        } catch (OperatorCreationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+        X509CertificateHolder holder = builder.build(contentSigner);
+
+        JcaX509CertificateConverter certificateConverter = new JcaX509CertificateConverter();
+        certificateConverter.setProvider(provider);
+        try {
+            return certificateConverter.getCertificate(holder);
+        } catch (CertificateException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+    }
+
+    public static X509Certificate generateCrossRoot(Provider provider, PrivateKey issuerPrivateKey, PublicKey issuerPublicKey, X500Name issuerSubject, PublicKey publicKey, X500Name subject) {
+        JcaX509ExtensionUtils utils = null;
+        try {
+            utils = new JcaX509ExtensionUtils();
+        } catch (NoSuchAlgorithmException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+
+        Date notBefore = LocalDate.now().toDate();
+        Date notAfter = LocalDate.now().plusYears(10).toDate();
+
+        JcaX509v3CertificateBuilder builder = new JcaX509v3CertificateBuilder(issuerSubject, BigInteger.valueOf(System.currentTimeMillis()), notBefore, notAfter, subject, publicKey);
+        try {
+            builder.addExtension(Extension.basicConstraints, true, new BasicConstraints(true));
+        } catch (CertIOException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+        try {
+            builder.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.cRLSign | KeyUsage.keyCertSign));
+        } catch (CertIOException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+        try {
+            builder.addExtension(Extension.subjectKeyIdentifier, false, utils.createSubjectKeyIdentifier(publicKey));
+        } catch (CertIOException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+        try {
+            builder.addExtension(Extension.authorityKeyIdentifier, false, utils.createAuthorityKeyIdentifier(issuerPublicKey));
+        } catch (CertIOException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+
+        String format = "";
+        if (issuerPrivateKey instanceof RSAPrivateKey) {
+            format = "RSA";
+        } else if (issuerPrivateKey instanceof ECPrivateKey) {
+            format = "ECDSA";
+        } else if (issuerPrivateKey instanceof DSAPrivateKey) {
+            format = "DSA";
+        }
+
+        int shaSize = 256;
+        JcaContentSignerBuilder contentSignerBuilder = new JcaContentSignerBuilder("SHA" + shaSize + "WITH" + format);
+        contentSignerBuilder.setProvider(provider);
+        ContentSigner contentSigner = null;
+        try {
+            contentSigner = contentSignerBuilder.build(issuerPrivateKey);
         } catch (OperatorCreationException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
