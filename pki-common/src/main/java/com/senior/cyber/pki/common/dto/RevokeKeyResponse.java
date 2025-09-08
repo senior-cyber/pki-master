74f67a3c65491428fc57604fe7265d8e484f4dbd
Socheat KHAUV <pkayjava@gmail.com>
2025-09-08 22:12:54 +0700

    add revoked api

--- pki-common/src/main/java/com/senior/cyber/pki/common/dto/RevokeKeyResponse.java
+++ pki-common/src/main/java/com/senior/cyber/pki/common/dto/RevokeKeyResponse.java
@@ -1,9 +1,22 @@
 package com.senior.cyber.pki.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Setter
 @Getter
 public class RevokeKeyResponse {
+
+    @JsonProperty("serverTime")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date serverTime;
+
+    public RevokeKeyResponse() {
+        this.serverTime = new Date();
+    }
+
 }
