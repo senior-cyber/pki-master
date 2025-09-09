0e3f9e5772273bb21dc41dcd7c5729f6d44f3c67
Socheat KHAUV <pkayjava@gmail.com>
2025-09-09 16:59:58 +0700

    more

--- pki-common/src/main/java/com/senior/cyber/pki/common/dto/MtlsClientGenerateRequest.java
+++ pki-common/src/main/java/com/senior/cyber/pki/common/dto/MtlsClientGenerateRequest.java
@@ -2,10 +2,11 @@ package com.senior.cyber.pki.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 @Getter
-@Service
+@Setter
 public class MtlsClientGenerateRequest {
 
     @JsonProperty("issuer")
